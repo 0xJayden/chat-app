b5d479a5f536d3affdd3aebdb06eb960e6e47bae
0xJayden <gannixcrypto@protonmail.com>
2022-09-27 14:38:37 -0700

    added env variables

--- components/users.tsx
+++ components/users.tsx
@@ -1,7 +1,4 @@
-import { prisma } from "../utils/prisma";
-
 export default function Users() {
-  // const users = prisma.user.findMany()
   return (
     <div className="flex justify-center p-5 border-l h-full min-w-[150px]">
       <h1>Users</h1>

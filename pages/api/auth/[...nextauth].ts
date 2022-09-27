0ee1d18aa4a650d2b6e2aaef54e1a4b7880c7692
0xJayden <gannixcrypto@protonmail.com>
2022-09-27 14:56:21 -0700

    fixed env variables

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,6 @@ import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../utils/prisma";
-// import { FirestoreAdapter } from '@next-auth/firebase-adapter';
 
 export const options: NextAuthOptions = {
   providers: [
@@ -24,15 +23,6 @@ export const options: NextAuthOptions = {
     }),
   ],
   adapter: PrismaAdapter(prisma),
-  // adapter: FirestoreAdapter({
-  //   apiKey: process.env.FIREBASE_API_KEY,
-  //   appId: process.env.FIREBASE_APP_ID,
-  //   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  //   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  //   projectId: process.env.FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.FIREBASE_STOREAGE_BUCKET,
-  //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  // }),
 };
 
 export default NextAuth(options);

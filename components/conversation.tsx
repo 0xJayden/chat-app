0dfd90369281dd5819e3467e71d561600a47a1cd
0xJayden <gannixcrypto@protonmail.com>
2022-10-29 21:10:24 -0700

    fixed error

--- components/conversation.tsx
+++ components/conversation.tsx
@@ -61,7 +61,7 @@ export default function ConversationWindow({
       onSuccess(data) {
         setCurrentConversation(data.conversation);
       },
-      // refetchInterval: 600,
+      refetchInterval: 600,
     }
   );
 
